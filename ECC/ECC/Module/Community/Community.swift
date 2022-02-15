f66bad6a1112e5aa54d6a8e7d3a9ac504ba9334f
wangjian81 <jian.wang81@pactera.com>
2022-02-15 18:29:53 +0800

    update

---
+++ ECC/ECC/Module/Community/Community.swift
@@ -0,0 +1,8 @@
+//
+//  Community.swift
+//  ECC_dev
+//
+//  Created by pactera on 2022/2/15.
+//
+
+import Foundation
---
+++ ECC/ECC/Module/StudyPlan/StudyPlan.swift
@@ -0,0 +1,8 @@
+//
+//  StydyPlan.swift
+//  ECC_dev
+//
+//  Created by pactera on 2022/2/15.
+//
+
+import Foundation
